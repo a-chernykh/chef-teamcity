130c16fe8079d42689f739b69c7e91f68d3803f3
Andrey Chernih <andrey.chernih@gmail.com>
2013-09-26 21:57:54 +0400

    Use better monit recipe

--- Berksfile
+++ Berksfile
@@ -1,1 +1,6 @@
 metadata
+
+cookbook 'apt'
+cookbook 'postgresql'
+cookbook 'database'
+cookbook 'monit', git: 'https://github.com/phlipper/chef-monit.git'
--- recipes/monit.rb
+++ recipes/monit.rb
@@ -1,13 +1,9 @@
 include_recipe 'monit::default'
 
-monitrc 'teamcity-server' do 
+monit_monitrc 'teamcity-server' do 
   variables pid_file: node[:teamcity][:server][:pid_file]
-  template_cookbook "teamcity"
-  template_source 'teamcity-server.monit.conf.erb'
 end
 
-monitrc 'teamcity-agent' do
+monit_monitrc 'teamcity-agent' do
   variables pid_file: node[:teamcity][:agent][:pid_file]
-  template_cookbook "teamcity"
-  template_source 'teamcity-agent.monit.conf.erb'
 end
