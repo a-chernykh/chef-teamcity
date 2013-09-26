d3f89b1c05a89f7e16603aee7b05dcc76fe05ff3
Andrey Chernih <andrey.chernih@gmail.com>
2013-09-26 22:31:26 +0400

    Restart TeamCity in case of HTTP error

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -1,7 +1,9 @@
 include_recipe 'monit::default'
 
 monit_monitrc 'teamcity-server' do 
-  variables pid_file: node[:teamcity][:server][:pid_file]
+  variables pid_file: node[:teamcity][:server][:pid_file],
+    host: node[:teamcity][:host],
+    port: node[:teamcity][:port]
 end
 
 monit_monitrc 'teamcity-agent' do
