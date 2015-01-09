ed07f73e2e59333eb6c38bb29fe3522140d8f168
Jon T Gretarsson <jon@relateiq.com>
2015-01-08 18:12:50 -0800

    Revert "Does lazy work here?"
    This reverts commit 80f537324d7bfb2c2c92dd4a745ae327526a7b69.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 default[:teamcity][:version]    = "9.0.1"
 default[:teamcity][:host]       = "localhost"
 default[:teamcity][:port]       = "8111"
-default[:teamcity][:url]        = lazy "http://#{node[:teamcity][:host]}:#{node[:teamcity][:port]}/"
+default[:teamcity][:url]        = "http://#{node[:teamcity][:host]}:#{node[:teamcity][:port]}/"
 default[:teamcity][:user]       = "teamcity"
 default[:teamcity][:path]       = "/usr/local/teamcity"
 default[:teamcity][:data_path]  = "/var/teamcity"
-default[:teamcity][:user_home]  = lazy "/home/#{node[:teamcity][:user]}"
+default[:teamcity][:user_home]  = "/home/#{node[:teamcity][:user]}"
 
-default[:teamcity][:file_name]     = lazy "TeamCity-#{node[:teamcity][:version]}.tar.gz"
-default[:teamcity][:download_url]  = lazy "http://download.jetbrains.com/teamcity/#{node[:teamcity][:file_name]}"
+default[:teamcity][:file_name]     = "TeamCity-#{node[:teamcity][:version]}.tar.gz"
+default[:teamcity][:download_url]  = "http://download.jetbrains.com/teamcity/#{node[:teamcity][:file_name]}"
 default[:teamcity][:download_path] = Chef::Config[:file_cache_path]
-default[:teamcity][:install_path]  = lazy "#{node[:teamcity][:path]}-#{node[:teamcity][:version]}"
+default[:teamcity][:install_path]  = "#{node[:teamcity][:path]}-#{node[:teamcity][:version]}"
